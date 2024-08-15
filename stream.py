d3d1525989dbdc0bd29fb77ed5aa6ef4df105613
faree777 <139102731+faree777@users.noreply.github.com>
2024-08-15 18:12:49 +0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -3,4 +3,5 @@ st.title("Hello streamlit")
 st.header("Learning Streamlit")
 name = st.text_input(" Enter your name")
 feedback = st.text_area("Enter your feedback")
-
+if st.button("Submit"):
+    st.write("Thanks for your feedback!")
